c018b0937d3f855157e530308945d407e134ff36
Ahmed <ahmed.abdi.ali@se.experis.com>
2022-08-04 19:25:00 +0200

    created DPS logic

---
+++ src/main/java/Attribute/DPS.java
@@ -0,0 +1,22 @@
+package Attribute;
+
+public class DPS {
+    public double Damage;
+    public double AttackSpeed;
+
+    public double getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(double damage) {
+        Damage = damage;
+    }
+
+    public double getAttackSpeed() {
+        return AttackSpeed;
+    }
+
+    public void setAttackSpeed(double attackSpeed) {
+        AttackSpeed = attackSpeed;
+    }
+}
