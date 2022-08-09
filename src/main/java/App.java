75dc12df385019f49db8b16263a7baeb348253e2
Ahmed <ahmed.abdi.ali@se.experis.com>
2022-08-09 18:38:45 +0200

    here it prints out the hero's strength,dexterity,intelligence toString,

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,11 @@
+import Model.Hero;
+import Model.UserHero;
 
 public class App {
     public static void main(String[] args) {
-
+        UserHero userHero = new UserHero(Hero.Mage);
+        System.out.println(userHero.getStats());
+        userHero.getStats().levelUp(userHero.getLevelUp());
+        System.out.println(userHero.getStats().toString());
     }
 }
